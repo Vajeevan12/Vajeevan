bc808e31ae19554ae0c20127b36c28f55cd3b362
Vajeevan12 <123532018+Vajeevan12@users.noreply.github.com>
2023-01-25 14:43:42 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,7 +44,7 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white text-sm md:text-lg px-4 py-2 border-none rounded-md ml-8"
                   href="#"
                 >
                   Resume
