aac31428144ca684d05700f3b047b329c89e34c8
Vajeevan12 <123532018+Vajeevan12@users.noreply.github.com>
2023-01-26 00:33:15 +0530

    Update project.js

--- pages/project.js
+++ pages/project.js
@@ -33,7 +33,7 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         
         </Head>
-        <main className=" bg-white px-10   dark:bg-gray-900 md:px-20 lg:px-40">
+        <main className=" bg-white px-5   dark:bg-gray-900 md:px-20 lg:px-40">
           <section className="" >
             <nav className="py-10 mb-12 flex justify-between text-black dark:text-white rou">
               <a className="font-burtons text-xs md:text-xl" href="/">Vajeevan R</a>
